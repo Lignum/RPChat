c1bb020323a7790b25a56e0520d0862fdd59e320
Lignum <aut@ism.pw>
2017-04-22 21:31:49 +0200

    Remove some empty space

--- src/main/java/me/lignum/rpchat/vars/MessageVars.java
+++ src/main/java/me/lignum/rpchat/vars/MessageVars.java
@@ -5,8 +5,6 @@ import me.lignum.rpchat.Utils;
 import org.spongepowered.api.text.Text;
 
 public class MessageVars {
-
-
     public static final MessageVar USER     = (p, r, m) -> Text.of(p.getName());
     public static final MessageVar MESSAGE  = (p, r, m) -> m;
     public static final MessageVar WORLD    = (p, r, m) -> Text.of(p.getWorld().getName());
